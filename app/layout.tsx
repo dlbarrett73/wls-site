724e542150e8742fd03b4d2b823b1e2576867a1a
dlbarrett73 <david@whitetaillandsolutions.com>
2025-09-17 13:02:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-// app/layout.tsx
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,11 +6,7 @@ export const metadata: Metadata = {
     "Properties engineered to attract and kill mature whitetails. Consulting and turnkey properties in Pennsylvania.",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
