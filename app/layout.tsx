2ec1adc34d0b6f4290605564229d9c9b17c83c4c
dlbarrett73 <david@whitetaillandsolutions.com>
2025-09-17 13:18:18 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Whitetail Land Solutions",
   description:
-    "Properties engineered to attract and kill mature whitetails. Consulting and turnkey properties in Pennsylvania.",
+    "Properties engineered to attract and kill mature whitetails. Consulting and turnkey properties in Pennsylvania."
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
